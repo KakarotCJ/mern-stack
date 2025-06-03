d4ad9d6ab6c6187a674b78882d52ca249ed2fc01
santhosh M <santhoshbca878@gmail.com>
2025-06-03 14:32:17 +0530

    create sign up api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -17,6 +17,6 @@ const userSchema = new mongoose.Schema({
     }
 },{ timestamps: true});
 
-const User = monggose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 
 export default User;
